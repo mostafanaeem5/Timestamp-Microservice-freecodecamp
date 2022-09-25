135b12fdf93424e3a72a1360e0db747e45fb9407
mostafanaeem5 <72983247+mostafanaeem5@users.noreply.github.com>
2022-09-25 20:04:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,29 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/api/', (req, res) => {
+    const time = new Date();
+    const unix = Date.parse(time);
+    res.json({ unix: unix, utc: time });
+});
+
 app.get('/api/:date', (req, res) => {
     let date = req.params.date;
+    let unixDate;
     if (date.length === 13) {
         date = Number(date);
+        unixDate = date;
+    } else {
+        unixDate = Date.parse(date);
     }
     const utcDate = new Date(date);
-    const unixDate = Date.parse(date);
-    if (unixDate != utcDate) {
-        res.json({ unix: unixDate, utc: utcDate.toUTCString() });
-    } else {
+    console.log(unixDate);
+    if (utcDate.toUTCString() === 'Invalid Date') {
+        res.json({ error: 'Invalid Date' });
+    } else if (unixDate === utcDate) {
         res.json({ unix: date, utc: utcDate.toUTCString() });
+    } else {
+        res.json({ unix: unixDate, utc: utcDate.toUTCString() });
     }
 });
 
